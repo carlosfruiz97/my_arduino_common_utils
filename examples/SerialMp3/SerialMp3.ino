3daf0ee62a399584e8edd7e93a37d57fe564227e
Carlos Fernandez <carlos.fernandez.ruiz@estudiantat.upc.edu>
2022-05-17 18:48:52 +0200

    failed attempt

---
+++ examples/SerialMp3/SerialMp3.ino
@@ -0,0 +1,11 @@
+#include "my_arduino_common_utils/mp3_serial.h"
+void setup()
+{
+  
+}
+
+
+void loop()
+{
+  
+}
---
+++ src/mp3_serial.h
@@ -0,0 +1,16 @@
+#ifndef SERIAL_MP3_H
+#define SERIAL_MP3_H
+
+#include "Arduino.h"
+
+
+class SerialMp3
+{
+	public:
+	SerialMp3()
+	{
+		
+	}
+};
+
+#endif
