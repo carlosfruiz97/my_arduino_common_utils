d5ab64bc6cff888e8d57c7ee5fef20ed7fcf6dbb
Carlos Fernandez <carlos.fernandez.ruiz@estudiantat.upc.edu>
2022-05-18 19:08:27 +0200

    refractor name

--- examples/SerialMp3/SerialMp3.ino
+++ examples/SerialMp3/SerialMp3.ino
@@ -9,11 +9,11 @@
 
 
 // Import Utils library and serial_mp3 module
-#include<my_arduino_common_utils.h>
-#include<serial_mp3/serial_mp3.h>
+#include <my_arduino_common_utils.h>
+#include <serial_mp3/serial_mp3.h>
 
 // Initiate Serial mp3 module
-SerialMp3 serial_mp3_;
+SerialMp3 mp3_player;
 
 
 void setup()
@@ -23,22 +23,20 @@ void setup()
 
   // Initate Stream class that will comunicate with mp3
   Serial1.begin(9600);
-  serial_mp3_.init(Serial1);
+  mp3_player.init(Serial1);
 
   // PLay from folder
   Serial.println("Play 01/01");
-//  serial_mp3_.playFromFolder(1, 1);
-
-
-
+//  mp3_player.playFromFolder(1, 1);
 }
+
 
 String msg, hex_resp_string;
 
 void loop()
 {
   // Mp3 State
-  if (serial_mp3_.decodeMP3Answer(msg, hex_resp_string) )
+  if (mp3_player.decodeMP3Answer(msg, hex_resp_string))
   {
     LOG("\nMp3 Response:    ");
     LOGN(msg);
@@ -50,53 +48,53 @@ void loop()
   // Serial Monitor comands
   if (Serial.available())
   {
-    char c = Serial.read();
+    char    c = Serial.read();
     uint8_t send_buffer[8];
     switch (c)
     {
-      case 'p':
-        // Pause!
-        serial_mp3_.pause();
-        break;
-      case 'r':
-        // Resume!
-        serial_mp3_.resume();
-        break;
-      case 'q':
-        // Stop!
-        serial_mp3_.stop();
-        break;
+    case 'p':
+      // Pause!
+      mp3_player.pause();
+      break;
+
+    case 'r':
+      // Resume!
+      mp3_player.resume();
+      break;
+
+    case 'q':
+      // Stop!
+      mp3_player.stop();
+      break;
 
 
-      case 'v':
-        // Volume!
-        serial_mp3_.setVolume(15);
-        break;
-      case 'V':
-        // Volume!
-        serial_mp3_.setVolume(30);
-        break;
+    case 'v':
+      // Volume!
+      mp3_player.setVolume(15);
+      break;
+
+    case 'V':
+      // Volume!
+      mp3_player.setVolume(30);
+      break;
 
 
-      case 'f':
-        // PLay from folder 01/002xxx.mp3!
-        serial_mp3_.playFromFolder(1, 2);
-        break;
-      case 'F':
-        // PLay from folder 03/003xxx.mp3!
-        serial_mp3_.playFromFolder(3, 3);
-        break;
+    case 'f':
+      // PLay from folder 01/002xxx.mp3!
+      mp3_player.playFromFolder(1, 2);
+      break;
+
+    case 'F':
+      // PLay from folder 03/003xxx.mp3!
+      mp3_player.playFromFolder(3, 3);
+      break;
 
 
 
-      case 'c':
-        // Cicle folder 02!
-        serial_mp3_.sendCommand(CMD_FOLDER_CYCLE, 2);
-        break;
-
-
+    case 'c':
+      // Cicle folder 02!
+      mp3_player.sendCommand(CMD_FOLDER_CYCLE, 2);
+      break;
     }
-
   }
-
 }
